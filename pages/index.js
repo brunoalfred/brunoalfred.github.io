c39de06f72cd29a7ed295a9546bd2fd792abeabd
brunoalfred <alfredybruno@gmail.com>
2021-03-04 18:26:14 +0300

    Added the site pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -9,6 +12,9 @@ export default function Home() {
         <title>brunoalfred</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+
+
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -23,12 +29,17 @@ export default function Home() {
         <div className={styles.grid}>
 
 
-          <a href="/posts/resume" className={styles.card}>
-            <h3>Resume &rarr;</h3>
-            <p>As i showcase my skills and experiences in brief</p>
-          </a>
+          <Link href="/posts/resume" >
+          
 
-          <Link href="#" >
+            <a className={styles.card}>
+              <h3>Resume &rarr;</h3>
+              <p>As i showcase my skills and experiences in brief</p>
+            </a>
+          
+          </Link>
+
+          <Link href="/social-media" >
             <a className={styles.card}>
               <h3>Socila Media &rarr;</h3>
               <p>How i appear before the rest of the world</p>
@@ -36,36 +47,35 @@ export default function Home() {
 
           </Link>
 
-          <a
-            href="#"
-            className={styles.card}
-          >
-            <h3>Projects &rarr;</h3>
-            <p>This is how I spend my time, always busy making </p>
-          </a>
 
-          <a
-            href="#"
-            className={styles.card}
-          >
-            <h3>Try me &rarr;</h3>
-            <p>
-              Incase of anything you can just reach me out instantly..
+          <Link href="/projects">
+
+            <a
+              
+              className={styles.card}
+            >
+              <h3>Projects &rarr;</h3>
+              <p>This is how I spend my time, always busy making </p>
+            </a>
+
+          </Link>
+
+         <Link href="/try-me">
+            <a
+              className={styles.card}
+            >
+              <h3>Try me &rarr;</h3>
+              <p>
+                Incase of anything you can just reach me out instantly..
             </p>
-          </a>
+            </a>
+         
+         </Link>
         </div>
+       
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://jasiriinc.tech"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '} JasiriInc
-          {/* <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> */}
-        </a>
-      </footer>
+<Footer/>
+     
     </div>
   )
 }
---
+++ pages/try-me.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header/Header'
+
+export default function TryMe(){
+    return(
+        <>
+        
+        <div>
+            <Header/>
+        </div>
+        
+        </>
+    )
+}
