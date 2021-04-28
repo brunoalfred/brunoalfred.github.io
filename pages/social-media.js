6714c6a499c357dfdf4ac1360720701c616ca3e9
brunoalfred <alfredybruno@gmail.com>
2021-04-28 23:56:39 +0300

    Added card on social media Page

--- pages/social-media.js
+++ pages/social-media.js
@@ -1,68 +1,81 @@
 import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
 import Link from 'next/link'
-
 import styles from '../styles/Home.module.css'
-
 export default function SocialMedia() {
     return (
         <div className={styles.container}>
-        
-
-
 
             <main className={styles.main}>
 
-            
+
                 <p className={styles.description}>
                     ask me about
           <code className={styles.code}>software Engineering Stack</code>
                 </p>
-
                 <div className={styles.grid}>
-
-
-                    <Link href="/posts/resume" >
-                        <a className={styles.card}>
-                            <h3>Resume &rarr;</h3>
-                            <p>As i showcase my skills and experiences in brief</p>
-                        </a>
-                    </Link>
-
                     <Link href="/social-media" >
                         <a className={styles.card}>
                             <h3>Socila Media &rarr;</h3>
                             <p>How i appear before the rest of the world</p>
                         </a>
                     </Link>
+                    
+                </div>
 
-
-                    <Link href="/projects">
-                        <a
-                            className={styles.card}
-                        >
-                            <h3>Projects &rarr;</h3>
-                            <p>This is how I spend my time, always busy making </p>
-                        </a>
-                    </Link>
-
-                    <Link href="/try-me">
-                        <a
-                            className={styles.card}
-                        >
-                            <h3>Try me &rarr;</h3>
-                            <p>
-                                Incase of anything you can just reach me out instantly..
+              
+                <div class="sm:flex flex-wrap justify-center items-center text-center gap-8">
+                    <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 bg-white mt-6  shadow-lg rounded-lg dark:bg-gray-800">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+                            Website Design
+                        </h3>
+                        <p class="text-md   py-4 font-semibold">
+                            Encompassing today’s website design technology to integrated and build solutions relevant to your business.
                         </p>
-                        </a>
-                    </Link>
-
+                    </div>
+                    <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 mt-6 sm:mt-16 md:mt-20 lg:mt-24 bg-white shadow-lg rounded-lg dark:bg-gray-800">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+                            Branding
+                        </h3>
+                        <p class="text-md py-4 font-semibold">
+                            Share relevant, engaging, and inspirational brand messages to create a connection with your audience.
+                        </p>
+                    </div>
+                    <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 mt-6  px-4 py-4 bg-white shadow-lg rounded-lg dark:bg-gray-800">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center mx-auto justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                <svg width="20" height="20" fill="currentColor" class="h-6 w-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+                            SEO Marketing
+                        </h3>
+                        <p class="text-md  py-4 font-semibold">
+                            Let us help you level up your search engine game, explore our solutions for digital marketing for your business.
+                        </p>
+                    </div>
                 </div>
 
             </main>
- 
             <Footer />
-
         </div>
     )
 }
