7788bb4070d64edd64bbd0a8df17f0b8dac73b6b
brunoalfred <alfredybruno@gmail.com>
2021-07-09 16:36:34 +0300

    Added some layouting to the Header

--- components/background/Background.js
+++ components/background/Background.js
@@ -1,10 +1,10 @@
 import styles from './Background.module.css'
 
 
-export default function Background({ children }) {
+export default function Background({ children, style }) {
     
     return (
-        <body className={styles.background}>{ children }</body>
+        <body className={styles.background} style={style}>{ children }</body>
     );
 }
 
--- layout/Base.js
+++ layout/Base.js
@@ -1,25 +1,17 @@
 import Background from "../components/background/Background";
-import Footer from "../components/footer/footer";
+import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
-
+import styles from './Base.module.css';
 
 
 export default function Base({ children }) {
     return (
-     
-     <Background>
-     
-                <Header />
-
-                <div>
-                    {children}
-                </div>
-
-                <Footer />
-     
+        <Background>
+            <Header />
+            <main>{children}</main>
+            <Footer />
         </Background>
     )
 };
-
 
 export { Base }
