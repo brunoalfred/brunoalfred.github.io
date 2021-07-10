36981f56142322dfb2b90e84041b9161a44104f1
brunoalfred <alfredybruno@gmail.com>
2021-07-10 14:50:16 +0300

    fixing the CORS error

--- components/background/Background.js
+++ components/background/Background.js
@@ -4,7 +4,7 @@ import styles from './Background.module.css'
 export default function Background({ children, style }) {
     
     return (
-        <body className={styles.background} style={style}>{ children }</body>
+        <div className={styles.background} style={style}>{ children }</div>
     );
 }
 
